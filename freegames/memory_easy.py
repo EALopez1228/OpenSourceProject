89808fb5dca26ad5c9fac7d59576e732926a282e
francisco <fbolanos@csu.fullerton.edu>
2021-05-04 18:47:38 -0700

    Comments on memory_easy

--- freegames/memory_easy.py
+++ freegames/memory_easy.py
@@ -1,13 +1,15 @@
-
+#creating an easy version of the memory game (4x4)
 from random import *
 from turtle import *
 from freegames import path
 
+#Creating the picture and tile objects
 car = path('car.gif')
 tiles = list(range(8)) * 2
 state = {'mark': None}
 hide = [True] * 16
 
+#Creating a square plus its size components
 def square(x, y):
     "Draw white square with black outline at (x, y)."
     up()
@@ -20,14 +22,17 @@ def square(x, y):
         left(90)
     end_fill()
 
+#Indexes of each tile
 def index(x, y):
     "Convert (x, y) coordinates to tiles index."
     return int((x + 200) // 100+ ((y + 200) // 100) * 4)
 
+#How many tiles
 def xy(count):
     "Convert tiles count to (x, y) coordinates."
     return (count % 4) * 100 - 200, (count // 4) * 100 - 200
 
+#Lets the user tap on tiles and reveal numbers, and give error if out of scope of tiles
 def tap(x, y):
     "Update mark and hidden tiles based on tap."
     spot = index(x, y)
@@ -53,17 +58,17 @@ def draw():
             square(x, y)
 
     mark = state['mark']
-
+#Out of bonds error
     if mark is not None and hide[mark]:
         x, y = xy(mark)
         up()
         goto(x + 2, y)
         color('black')
-        write(tiles[mark], font=('Arial', 30, 'normal'))
+        write(tiles[mark], font=('Arial', 30, 'normal'))  
 
     update()
     ontimer(draw, 100)
-
+#Shuffling of tile numbers and creating the viewer
 shuffle(tiles)
 setup(420, 420, 370, 0)
 addshape(car)
