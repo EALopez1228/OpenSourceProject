cc67104375dd8075e7e779c21b03fa4df21837e8
EALopez1228 <32082563+EALopez1228@users.noreply.github.com>
2021-05-10 20:37:37 -0700

    Labels added
    Added labels to menu

--- freegames/mainmenu.py
+++ freegames/mainmenu.py
@@ -26,6 +26,30 @@ tiles = [
     [750, -450]
 ]
 
+games = [
+    "Ant",
+    "Bagels",
+    "Bounce",
+    "Cannon",
+    "Connect",
+    "Crypto",
+    "Fidget",
+    "Flappy",
+    "Guess",
+    "Life",
+    "Maze",
+    "Memory",
+    "Minesweep",
+    "Pacman",
+    "Pain",
+    "Pong",
+    "Simon",
+    "Snake",
+    "TicTacToe",
+    "Tiles",
+    "Tron"
+]
+
 def checkBox(clicked):
     index = 0;
     for i in tiles:
@@ -38,47 +62,47 @@ def RunGame(boxClicked):
     cwd = os.path.dirname(os.path.realpath(__file__))
 
     if(boxClicked == 0):
-        os.system('python3 ' + cwd + ' /ant.py')
+        os.system('python3 ' + cwd + '/ant.py')
     elif(boxClicked == 1):
-        os.system('python3 ' + cwd + ' /bagels.py')
+        os.system('python3 ' + cwd + '/bagels.py')
     elif(boxClicked == 2):
-        os.system('python3 ' + cwd + ' /bounce.py')
+        os.system('python3 ' + cwd + '/bounce.py')
     elif(boxClicked == 3):
-        os.system('python3 ' + cwd + ' /cannon.py')
+        os.system('python3 ' + cwd + '/cannon.py')
     elif(boxClicked == 4):
-        os.system('python3 ' + cwd + ' /connect.py')
+        os.system('python3 ' + cwd + '/connect.py')
     elif(boxClicked == 5):
-        os.system('python3 ' + cwd + ' /crypto.py')
+        os.system('python3 ' + cwd + '/crypto.py')
     elif(boxClicked == 6):
-        os.system('python3 ' + cwd + ' /fidget.py')
+        os.system('python3 ' + cwd + '/fidget.py')
     elif(boxClicked == 7):
-        os.system('python3 ' + cwd + ' /flappy.py')
+        os.system('python3 ' + cwd + '/flappy.py')
     elif(boxClicked == 8):
-        os.system('python3 ' + cwd + ' /guess.py')
+        os.system('python3 ' + cwd + '/guess.py')
     elif(boxClicked == 9):
-        os.system('python3 ' + cwd + ' /life.py')
+        os.system('python3 ' + cwd + '/life.py')
     elif(boxClicked == 10):
-        os.system('python3 ' + cwd + ' /maze.py')
+        os.system('python3 ' + cwd + '/maze.py')
     elif(boxClicked == 11):
-        os.system('python3 ' + cwd + ' /memory.py')
+        os.system('python3 ' + cwd + '/memory.py')
     elif(boxClicked == 12):
-        os.system('python3 ' + cwd + ' /minesweeper.py')
+        os.system('python3 ' + cwd + '/minesweeper.py')
     elif(boxClicked == 13):
-        os.system('python3 ' + cwd + ' /pacman.py')
+        os.system('python3 ' + cwd + '/pacman.py')
     elif(boxClicked == 14):
-        os.system('python3 ' + cwd + ' /paint.py')
+        os.system('python3 ' + cwd + '/paint.py')
     elif(boxClicked == 15):
-        os.system('python3 ' + cwd + ' /pong.py')
+        os.system('python3 ' + cwd + '/pong.py')
     elif(boxClicked == 16):
-        os.system('python3 ' + cwd + ' /simonsays.py')
+        os.system('python3 ' + cwd + '/simonsays.py')
     elif(boxClicked == 17):
-        os.system('python3 ' + cwd + ' /snake.py')
+        os.system('python3 ' + cwd + '/snake.py')
     elif(boxClicked == 18):
-        os.system('python3 ' + cwd + ' /tictactoe.py')
+        os.system('python3 ' + cwd + '/tictactoe.py')
     elif(boxClicked == 19):
-        os.system('python3 ' + cwd + ' /tiles.py')
+        os.system('python3 ' + cwd + '/tiles.py')
     elif(boxClicked == 20):
-        os.system('python3 ' + cwd + ' /tron.py')
+        os.system('python3 ' + cwd + '/tron.py')
 
 
 def grid():
@@ -104,6 +128,13 @@ def grid():
     square(450, -450, 300, 'blue')
     square(750, -450, 300, 'red')
 
+def draw():
+    color('black')
+    for i, j in zip(tiles, games):
+        goto(i[0], i[1] + 150)
+        text = j
+        write(text, font=("Terminal", 40, 'normal'))
+
 
 def tap(x, y):
     "Respond to screen tap."
@@ -126,5 +157,6 @@ setup(2100,900, 370, 0)
 hideturtle()
 tracer(False)
 grid()
+draw()
 onscreenclick(start)
 done()
